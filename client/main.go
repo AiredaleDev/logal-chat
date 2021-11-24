96a627868f0a49253de115edc903b8b8738138f4
AiredaleDev <airedaledev@protonmail.com>
2021-11-24 16:03:18 -0500

    moving to laptop

--- client/main.go
+++ client/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+    "bufio"
     "fmt"
-    // "net"
+    "log"
+    "net"
+    "os"
 )
 
 func main() {
-    fmt.Println("Hello, Golang!")
+    conn, err := net.Dial("tcp", ":6969")
+    defer conn.Close()
+    if err != nil {
+        log.Fatalln("ERROR:", err)
+    }
+
+    messageBox := bufio.NewScanner(os.Stdin)
+    reply := []byte{}
+    for messageBox.Scan() {
+        message := messageBox.Text()
+        if message == ":q" {
+            break
+        }
+        fmt.Fprintln(conn, message)
+        conn.Read(reply)
+        fmt.Println(string(reply))
+    }
+
+    if err := messageBox.Err(); err != nil {
+        log.Fatalln("ERROR:", err)
+    } else {
+        fmt.Println("Quitting...")
+    }
 }
--- server/main.go
+++ server/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "log"
     "net"
+    "os"
 )
 
 func main() {
-    fmt.Println("Hello, Golang")
+    // wow that was easy
+    server, err := net.Listen("tcp", ":6969")
+    if err != nil {
+        log.Fatalln("ERROR:", err)
+    }
 
-    tcpServer := net.TCPListener{}
+    // TODO: Give us a CLI to interact with the server
+    // go func() {}()
+
+    for {
+        conn, err := server.Accept()
+        if err != nil {
+            fmt.Fprintln(os.Stderr, "Accept ERROR:", err)
+            continue
+        }
+
+        go func(conn net.Conn) {
+            from := bufio.NewScanner(conn)
+            var msgBuffer string
+            for from.Scan() {
+                msgBuffer = from.Text()
+                fmt.Println("Got message:", msgBuffer)
+                fmt.Fprintln(conn, "Echo:", msgBuffer)
+            }
+        }(conn)
+    }
 }
