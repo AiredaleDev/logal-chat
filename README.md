1953c3ff963ba95ce1cc3cb3b81d3e43d054260d
Cameron <airedaledev@protonmail.com>
2021-11-25 15:24:49 -0500

    move to desktop

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Yet another basic local chat program to learn socket programming.
 * TUI with nice colors (wow! riveting!)
 * You can talk to people
 * Maybe I'll give a GTK gui or something
+* Or NCurses
+* Probably going to encrypt the traffic too because who likes being spied on?
--- server/main.go
+++ server/main.go
@@ -8,6 +8,52 @@ import (
     "os"
 )
 
+type message struct {
+    senderID int
+    contents string
+}
+
+type client struct {
+    name string
+    conn net.Conn
+}
+
+var (
+    messages chan message = make(chan message)
+    clients []client
+)
+
+func broadcast() {
+    for {
+        newestMessage := <-messages
+        for clientID, client := range clients {
+            fmt.Println("ClientID:", clientID)
+            if newestMessage.senderID == clientID {
+                continue
+            }
+            fmt.Fprintln(client.conn, newestMessage.contents)
+        }
+    }
+}
+
+func newActiveConnection(id int, userInfo *client) {
+    from := bufio.NewScanner(userInfo.conn)
+    defer userInfo.conn.Close()
+
+    fmt.Fprintf(userInfo.conn, "Welcome to the server, %s!\n", userInfo.name)
+    messages<- message{id, userInfo.name + " has joined the server."}
+
+    var msgBuffer string
+    for from.Scan() {
+        msgBuffer = from.Text()
+        fmt.Println("Got message:", msgBuffer)
+        messages<- message{id, msgBuffer}
+    }
+    if err := from.Err(); err != nil {
+        fmt.Println("Conneciton no.", id, "- Well, shit")
+    }
+}
+
 func main() {
     // wow that was easy
     fmt.Println("Server starting...")
@@ -19,21 +65,18 @@ func main() {
     // TODO: Give us a CLI to interact with the server
     // go func() {}()
 
-    for {
+    go broadcast()
+    for clientNo := 0; true; clientNo++ {
         conn, err := server.Accept()
         if err != nil {
             fmt.Fprintln(os.Stderr, "Accept ERROR:", err)
             continue
         }
+        clients = append(clients, client{"", conn})
+        fmt.Println("Client created. ID:", clientNo)
+        fmt.Println("Clients:", clients)
 
-        go func(conn net.Conn) {
-            from := bufio.NewScanner(conn)
-            var msgBuffer string
-            for from.Scan() {
-                msgBuffer = from.Text()
-                fmt.Println("Got message:", msgBuffer)
-                fmt.Fprintln(conn, "Echo:", msgBuffer)
-            }
-        }(conn)
+        // Each connection gets its own goroutine
+        go newActiveConnection(clientNo, &clients[clientNo])
     }
 }
